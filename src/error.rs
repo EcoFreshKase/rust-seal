7ea6baeabcd15d69b70a971fb67367b88896789d
Alexander Jablonowski <alexander.jablonowski@telekom.de>
2025-07-31 13:42:24 +0200

    add error type for oqs algorithm string conversion

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,14 @@ use std::fmt::Display;
 
 pub enum RustSealError {
     CliInvalidArgument(String),
+    OqsError(String),
 }
 
 impl Display for RustSealError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             RustSealError::CliInvalidArgument(msg) => write!(f, "Invalid CLI argument: {}", msg),
+            RustSealError::OqsError(msg) => write!(f, "OQS error: {}", msg),
         }
     }
 }
