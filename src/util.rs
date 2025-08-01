1d79d12bfc814f9bc4b54a406961421e735708f0
Alexander Jablonowski <alexander.jablonowski@telekom.de>
2025-08-01 14:55:48 +0200

    fix: don't overwrite already existing files

--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ use std::{fs::File, io::Write, path::PathBuf};
 use anyhow::{Context, Result};
 
 pub fn create_file_with_content(file_path: &PathBuf, content: &[u8]) -> Result<PathBuf> {
-    let mut file = File::create(file_path).context("Failed to create file")?;
+    let mut file = File::create_new(file_path).context("Failed to create file")?;
     file.write_all(content)
         .context("Failed to write content to file")?;
     Ok(file_path.to_path_buf())
